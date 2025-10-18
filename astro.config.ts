c6746bb5816518aff5001929faec18b7e452741c
sakakibara yuuki <sakakilabo0000@gmail.com>
2025-10-19 06:08:09 +0900

    feat: Add db integration and test blog collection configuration

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs({ devtools: true })]
+  integrations: [solidJs({ devtools: true }), db()]
 });
