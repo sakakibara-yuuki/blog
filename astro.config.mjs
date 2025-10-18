768b4d745669f2e82a376c11ca37832d98ce06ea
sakakibara yuuki <sakakilabo0000@gmail.com>
2025-10-19 04:34:50 +0900

    setup: solid devtools

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs({ devtools: true })]
 });
