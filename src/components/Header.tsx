8d83f581734ff7c79f996154485d367da7cfab1f
sakakibara yuuki <sakakilabo0000@gmail.com>
2025-10-21 07:09:26 +0900

    feat: Add ThemeProvider component to Layout.astro file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,31 @@ export const Header = () => {
   const $theme = useStore(theme);
 
   const toggleTheme = () => {
-    theme.set($theme() == Theme.dark ? Theme.light : Theme.dark)
+    theme.set($theme() == Theme.dark ? Theme.light : Theme.dark);
   }
 
   return (
     <Button onClick={toggleTheme}>{$theme()}</Button>
   )
 }
+
+// -export const ThemeProvider: ParentComponent = (props) => {
+// -  const safeChildren = children(() => props.children);
+// -  const [theme, setTheme] = createSignal<Theme>(Theme.dark);
+// -  const toggleTheme = () => {
+// -    setTheme(prev => prev == Theme.dark ? Theme.light : Theme.dark)
+// -  }
+// -
+// -  createEffect(() => {
+// -    document.documentElement.setAttribute('data-theme', theme())
+// -  })
+// -
+// -  return (
+// -    <ThemeContext.Provider value={[theme, toggleTheme]}>
+// -      <div classList={{ "dark--theme": theme() == Theme.dark, "light--theme": theme() >
+// -        {safeChildren()}
+// -      </div>
+// -    </ThemeContext.Provider>
+// -  )
+// -}
+//
---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import type { ParentComponent } from 'solid-js';
+import { useStore } from '@nanostores/solid';
+import { createEffect } from 'solid-js'
+import { Theme, theme } from '@store/theme';
+
+export const ThemeProvider: ParentComponent = (props) => {
+
+  const $theme = useStore(theme);
+
+  return (
+    <div classList={{
+      'dark-theme': $theme() == Theme.dark,
+      'light-theme': $theme() == Theme.light,
+      'theme-container': true
+    }}>
+      {props.children}
+    </div>
+  )
+}
