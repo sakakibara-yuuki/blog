af2ba0589882baefb13d3d58af8b25bd3a37b5ea
sakakibara yuuki <sakakilabo0000@gmail.com>
2025-10-21 01:57:59 +0900

    feat: Add theme toggling functionality

--- src/components/ThemeContext.tsx
+++ src/components/ThemeContext.tsx
@@ -1,5 +1,5 @@
 import '@components/Theme.css';
-import { type ParentComponent, children } from 'solid-js';
+import { type ParentComponent, children, createSignal, type Accessor, createEffect } from 'solid-js';
 import { createContext, useContext } from 'solid-js';
 
 export enum Theme {
@@ -7,16 +7,30 @@ export enum Theme {
   light = "light"
 }
 
-export const ThemeContext = createContext<Theme>(Theme.dark)
-export const useThemeContext =  () => useContext(ThemeContext)
-export const ThemeProvider: ParentComponent<{ theme: Theme }> = (props) => {
+type ThemeContextType = [Accessor<Theme>, () => void]
+
+export const ThemeContext = createContext<ThemeContextType>([() => Theme.dark, () => { }])
+
+
+export const ThemeProvider: ParentComponent = (props) => {
   const safeChildren = children(() => props.children);
+  const [theme, setTheme] = createSignal<Theme>(Theme.dark);
+  const toggleTheme = () => {
+    setTheme(prev => prev == Theme.dark ? Theme.light : Theme.dark)
+  }
+
+  createEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme())
+  })
 
   return (
-    <ThemeContext.Provider value={props.theme}>
-      <div classList={{ "dark--theme": props.theme == Theme.dark, "light--theme": props.theme == Theme.light }}>
+    <ThemeContext.Provider value={[theme, toggleTheme]}>
+      <div classList={{ "dark--theme": theme() == Theme.dark, "light--theme": theme() == Theme.light }}>
         {safeChildren()}
       </div>
     </ThemeContext.Provider>
   )
 }
+
+
+export const useThemeContext = () => useContext(ThemeContext)
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,8 @@
-import { createSignal } from 'solid-js';
-import { Theme, ThemeProvider, useThemeContext } from '@components/ThemeContext';
+import { useThemeContext } from '@components/ThemeContext';
 import { Button } from '@components/Button';
 
 export const Header = () => {
-
-  const initTheme = useThemeContext()
-  const [theme, setTheme] = createSignal<Theme>(initTheme);
-  const toggleTheme = () => setTheme(theme() == Theme.dark ? Theme.light : Theme.dark)
+  const [theme, toggleTheme] = useThemeContext();
 
   return (
     <Button onClick={toggleTheme}>{theme()}</Button>
